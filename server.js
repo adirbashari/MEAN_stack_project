d87b34abd71111a15385499b17cace98e319e628
Adir Bashari <bashariadir@gmail.com>
2021-08-13 22:42:45 +0300

    update server fille and adding nodemon

--- server.js
+++ server.js
@@ -1,7 +1,57 @@
-const http = require('http');
-const port= process.env.Potrt || 3000;
+const app = require("./backend/app");
+const debug = require("debug")("node-angular");
+const http = require("http");
 
-const server=http.createServer((req, res)=> {
-  res.end('Hello World!');
-});
+
+//check if port is valid number
+const normalizePort = val => {
+  var port = parseInt(val, 10);
+
+  if (isNaN(port)) {
+    // named pipe
+    return val;
+  }
+
+  if (port >= 0) {
+    // port number
+    return port;
+  }
+
+  return false;
+};
+
+//error handling
+const onError = error => {
+  if (error.syscall !== "listen") {
+    throw error;
+  }
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  switch (error.code) {
+    case "EACCES":
+      console.error(bind + " requires elevated privileges");
+      process.exit(1);
+      break;
+    case "EADDRINUSE":
+      console.error(bind + " is already in use");
+      process.exit(1);
+      break;
+    default:
+      throw error;
+  }
+};
+
+//tell that the server start listening
+const onListening = () => {
+  const addr = server.address();
+  const bind = typeof port === "string" ? "pipe " + port : "port " + port;
+  debug("Listening on " + bind);
+};
+
+const port = normalizePort(process.env.PORT || "3000");
+app.set("port", port);
+
+const server = http.createServer(app);
+server.on("error", onError);
+server.on("listening", onListening);
+//starting the server
 server.listen(port);
