6b4cf6a5f5cb88691141cc1e5e0605d770627bc0
Adir Bashari <bashariadir@gmail.com>
2021-08-14 15:46:48 +0300

    add get and delete posts finish

--- backend/app.js
+++ backend/app.js
@@ -1,6 +1,17 @@
 const express = require("express");
 const Post = require('./models/post');
 const app = express();
+const mongoose = require("mongoose")
+
+
+mongoose.connect("mongodb+srv://adir:adirbashari@cluster0.pad74.mongodb.net/mean-stack?retryWrites=true&w=majority")
+.then(() => {
+  console.log("Connected to database!");
+})
+.catch(() => {
+  console.log("Connection failed!");
+});
+
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
@@ -24,30 +35,31 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content
   });
-  console.log(post);
-  res.status(201).json({
-    message: 'Post added successfully'
+  post.save().then(createdPost=>{
+    res.status(201).json({
+      message: 'Post added successfully',
+      postId:createdPost._id
+    });
   });
 });
 
 
 app.get("/api/posts", (req, res, next) => {
-  const posts = [
-    {
-      id: "fadf12421l",
-      title: "First server-side post",
-      content: "This is coming from the server"
-    },
-    {
-      id: "ksajflaj132",
-      title: "Second server-side post",
-      content: "This is coming from the server!"
-    }
-  ];
-  res.status(200).json({
+  Post.find().then(documents=>{
+    res.status(200).json({
     message: 'Posts fetched succesfully!',
-    posts: posts
+    posts: documents
+  });});
+
+});
+
+app.delete("/api/posts/:id",(req,res,next)=>{
+  console.log(req.params.id);
+  Post.deleteOne({_id:req.params.id}).then(result =>{
+    console.log(result);
+    res.status(200).json({message:"Post deleted"});
   });
 });
+
 
 module.exports = app;
