04042f02331d261addbed78b6c0b91960ac8ad6d
Adir Bashari <bashariadir@gmail.com>
2021-09-02 16:37:28 +0300

    adding pagination to app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -10,8 +10,7 @@ import {MatButtonModule} from '@angular/material/button';
 import {MatToolbarModule} from '@angular/material/toolbar';
 import {MatExpansionModule} from '@angular/material/expansion';
 import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
-import { FormsModule } from '@angular/forms';
-
+import {MatPaginatorModule} from '@angular/material/paginator';
 
 import { AppComponent } from "./app.component";
 import { PostCreateComponent } from "./posts/post-create/post-create.component";
@@ -37,8 +36,8 @@ import { AppRoutingModule } from "./app-routing.module";
     MatToolbarModule,
     MatExpansionModule,
     MatProgressSpinnerModule,
-    HttpClientModule,
-    FormsModule
+    MatPaginatorModule,
+    HttpClientModule
   ],
   providers: [],
   bootstrap: [AppComponent]
